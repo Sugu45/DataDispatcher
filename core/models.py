a97b3dae5e5ed5607116f63a2c7ef5a74098dd27
Sugu45 <sugumar199842@gmail.com>
2024-06-15 23:14:00 +0530

    Updated File

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import uuid
+from django.core.exceptions import ValidationError
+
 
 class Account(models.Model):
     email = models.EmailField(unique=True)
@@ -8,11 +10,14 @@ class Account(models.Model):
     app_secret_token = models.CharField(max_length=255, default=uuid.uuid4().hex)
     website = models.URLField(blank=True, null=True)
 
-
+def validate_http_method(value):
+    allowed_methods = ['GET', 'POST', 'PUT']
+    if value not in allowed_methods:
+        raise ValidationError(f"{value} is not a valid HTTP method. Allowed methods are: {', '.join(allowed_methods)}")
 class Destination(models.Model):
     account = models.ForeignKey(Account, related_name='destinations', on_delete=models.CASCADE)
     url = models.URLField()
-    http_method = models.CharField(max_length=10, choices=[('GET', 'GET'), ('POST', 'POST'), ('PUT', 'PUT')])
+    http_method = models.CharField(max_length=10)
     headers = models.JSONField()
 
 
